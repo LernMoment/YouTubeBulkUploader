f80ebba81901692c5f1aa57169e2d9c1afe5bafe
Jan Suchotzki <jan@lernmoment.de>
2017-09-11 15:21:08 +0200

    Updates to .NET Core 2.0

--- YouTubeBulkUploader/Program.cs
+++ YouTubeBulkUploader/Program.cs
@@ -7,6 +7,7 @@ namespace YouTubeBulkUploader
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
